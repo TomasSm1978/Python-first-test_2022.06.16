9e5a06b329afaf73fd3aff28320b2bf2d789bb53
TomasSm1978 <99352018+TomasSm1978@users.noreply.github.com>
2022-06-20 19:58:45 +0300

    Pakoreguotas sprendimas

--- test1.py
+++ test1.py
@@ -20,16 +20,23 @@ users = [
 
 
 def filter_dog_owners(x):
-    users_filtered = list(filter(lambda d: d['hasDog'] == True, x))
-    print(f"'users', kurie turi augintinį, sąrašas masyve: {users_filtered}")
-    print("Sarašas 'users', kurie turi augintinį, tik pateikiamas ne masyvo forma:")
+    print(f"'users', kurie turi augintinį, sąrašas:")
+    users_has_dog_list = []
+    users_filtered = list(filter(lambda d: d['hasDog'] is True, x))
     for user in users_filtered:
+        users_has_dog_list.append(user['name'])
         print(user['name'])
+    print(f"'users', kurie turi augintinį, sąrašas masyve: {users_has_dog_list}")
 
 
 def filter_adults(x):
+    print(f"'Suaugusiųjų 'users' sąrašas:")
+    users_adult = []
     users_filtered = list(filter(lambda d: d['age'] >= 18, x))
-    print(f"Suaugę 'users', pateikiami masyve: {users_filtered}")
+    for user in users_filtered:
+        users_adult.append(user['name'])
+        print(user['name'])
+    print(f"Suaugę 'users', pateikiami masyve: {users_adult}")
 
 
 filter_dog_owners(users)
